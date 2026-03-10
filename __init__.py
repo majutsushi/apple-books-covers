cc3d5fcb734fa3f9aa5e36677477f00eb0c42c9b
Jan Larres <jan@majutsushi.net>
2026-03-10 23:01:39 +1300

    Version 1.1.1

--- __init__.py
+++ __init__.py
@@ -55,7 +55,7 @@ class AppleBooksCovers(Source):
     description = "Downloads high resolution covers from the Apple Books store"
     capabilities = frozenset(["cover"])
     author = "Jan Larres"
-    version = (1, 1, 0)
+    version = (1, 1, 1)
     can_get_multiple_covers = True
 
     _countries = load_countries()
