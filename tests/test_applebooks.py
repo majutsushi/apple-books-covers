838aa034baa6027908c5d64f2a24373e954f4d4b
Jan Larres <jan@majutsushi.net>
2026-03-10 22:59:45 +1300

    Improve tests by verifying artwork URLs

--- tests/test_applebooks.py
+++ tests/test_applebooks.py
@@ -5,6 +5,8 @@ import os
 import sys
 import typing
 import unittest
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
 
 test_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path = [test_dir, *sys.path]
@@ -22,6 +24,13 @@ class TestAppleBooksCovers(unittest.TestCase):
         self.plugin = AppleBooksCovers(None)
         self.plugin.log = default_log  # type: ignore[reportAttributeAccessIssue]
 
+    def assertRequest(self, url: str) -> None:
+        req = Request(url, method="HEAD")  # noqa: S310
+        try:
+            urlopen(req)  # noqa: S310
+        except HTTPError:
+            self.fail(f"URL failed: {url}")
+
     def test_isbn_lookup(self):
         self.plugin.prefs[self.plugin.KEY_COUNTRY] = "US"
         self.plugin.prefs[self.plugin.KEY_ADDITIONAL_COUNTRY] = None
@@ -35,6 +44,7 @@ class TestAppleBooksCovers(unittest.TestCase):
         self.assertGreaterEqual(len(results), 1)
         self.assertEqual(results[0].author, "Michael Connelly")
         self.assertEqual(results[0].title, "The Fifth Witness")
+        self.assertRequest(results[0].artwork_url)
 
     def test_search(self):
         self.plugin.prefs[self.plugin.KEY_COUNTRY] = "US"
@@ -50,6 +60,7 @@ class TestAppleBooksCovers(unittest.TestCase):
         self.assertEqual(results[0].title, "A Game of Thrones")
         self.assertIn("George R.R. Martin", results[1].author)
         self.assertIn("A Game of Thrones", results[1].title)
+        self.assertRequest(results[0].artwork_url)
 
     def test_search2(self):
         self.plugin.prefs[self.plugin.KEY_COUNTRY] = "US"
@@ -63,6 +74,7 @@ class TestAppleBooksCovers(unittest.TestCase):
         self.assertGreaterEqual(len(results), 1)
         self.assertEqual(results[0].author, "N. K. Jemisin")
         self.assertEqual(results[0].title, "The Fifth Season")
+        self.assertRequest(results[0].artwork_url)
 
     def test_search_multi_author(self):
         self.plugin.prefs[self.plugin.KEY_COUNTRY] = "US"
@@ -78,6 +90,8 @@ class TestAppleBooksCovers(unittest.TestCase):
         self.assertEqual(results[0].title, "The Three-Body Problem")
         self.assertEqual(results[1].author, "Cixin Liu, Ken Liu & Joel Martinsen")
         self.assertEqual(results[1].title, "The Three-Body Problem Series")
+        self.assertRequest(results[0].artwork_url)
+        self.assertRequest(results[1].artwork_url)
 
     def test_multi_store(self):
         self.plugin.prefs[self.plugin.KEY_COUNTRY] = "US"
@@ -93,6 +107,8 @@ class TestAppleBooksCovers(unittest.TestCase):
         self.assertEqual(results[0].title, "Dark in Death")
         self.assertEqual(results[1].author, "J. D. Robb")
         self.assertEqual(results[1].title, "Dark in Death")
+        self.assertRequest(results[0].artwork_url)
+        self.assertRequest(results[1].artwork_url)
 
 
 if __name__ == "__main__":
